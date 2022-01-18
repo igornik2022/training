6790837ee4aebf2821ea3ea916e44a6343f3d55f
Igor Sickorsky <igoryok.2@yandex.ru>
2022-01-18 21:45:57 +0300

    #TDP-4 Добавление ХП к таблицу Persons
    Создание процедур для работы с таблицеу [dbo].[Persons]:
    - [dbo].[sp_Persons_Select] - выборка информации по ФЛ;
    - [dbo].[sp_Persons_INsert] - добавление нового ФЛ;
    - [dbo].[s-_Persons_Update] - обновление ФЛ.

---
+++ TextDatabaseProjecttt/sp_Persons_Update.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[sp_Persons_Update]
+	@Id int --Идентификатор лица
+	,@Surname nvarchar(50) --Фамилия
+	,@Name nvarchar (50) --Имя
+	,@Patronymic nvarchar (50) --Отчество
+	,@DateOfBirth date --Дата рождения
+AS
+BEGIN
+	-- SET NOCOUNT ON запрещает вывод количества строк, на которые влияет
+	-- инструкция Trfncact-SQL, в составе раезультаирующих данных
+	SET NOCOUNT ON;
+
+	UPDATE [dbo].[Persons]
+		SET [Surname] = @Surname
+			,[Name] = @Name
+			,[Patronymic] = @Patronymic
+			,[DateOfBirth] = @DateOfBirth
+		WHERE [Id] = @Id;
+END
